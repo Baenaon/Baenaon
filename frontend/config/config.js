679911878a5ee505c10e87c4a523b32323c4c7a6
donghyeok1 <ec2-user@ip-172-31-42-52.ap-northeast-2.compute.internal>
2022-10-19 14:48:35 +0000

    test

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,2 +1,2 @@
-export const backUrl = "http://127.0.0.1:8000";
-export const frontUrl = "http://127.0.0.1:3000";
+export const backUrl = "http://3.39.67.158:8080";
+export const frontUrl = "http://baenaon.com";

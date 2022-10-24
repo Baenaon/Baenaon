26b6f7cc661bf150ca29db6d690e2501e095cf8e
Ubuntu <ubuntu@ip-172-31-7-162.ap-northeast-2.compute.internal>
2022-10-24 16:55:38 +0000

    주변 이웃 게시글 업데이트

--- frontend/pages/usernearpost.js
+++ frontend/pages/usernearpost.js
@@ -17,7 +17,8 @@ const UserNearPost = ({}) => {
   const { postcards, setpostcards } = useState([]);
   const { token, setToken } = useState("");
   const map1 = new Map();
-  const [mainPosts, setPost] = useState({});
+  const [mainPosts, setPost] = useState([]);
+  const [flag, setFlag] = useState(false);
   map1.set("a", "한식");
   map1.set("b", "중식");
   map1.set("c", "일식");
@@ -26,59 +27,64 @@ const UserNearPost = ({}) => {
   map1.set("f", "카페/디저트");
   useEffect(() => {
     const access = window.localStorage.getItem("access_token");
+    if (flag == false){
+      const test = axios
+        .get(`${backUrl}/api/posts/`, {
+          headers: {
+            Authorization: `Bearer ${access}`,
+          },
+        })
+        .then(function (response) {
+          setPost(response.data.result);
+          if (mainPosts.length > 0){
+            setFlag(true);
+          }
+        });
+    }
+  }, [mainPosts]);
 
-    const test = axios
-      .get(`${backUrl}/api/posts/`, {
-        headers: {
-          Authorization: `Bearer ${access}`,
-        },
-      })
-      .then(function (response) {
-	setPost(response.data.result);
-	console.log("진짜 마지막",mainPosts)
-      })
-	  console.log("밖에서 보이나?", mainPosts);
-});
-
-return (
-    <div> {mainPosts ? (
+  return (
+    <div>
+      {" "}
+      {mainPosts && mainPosts.length > 0 ? (
         <div>
-        <Header />
-        <div class="">
+          <Header />
+          <div class="">
             <div class="flex justify-center m-3 ">
-            <div class=" text-2xl">
+              <div class=" text-2xl">
                 <div class="font-custom h-7 flex m-8 ">
-                <div>내 주위 이웃들의 게시글</div>{" "}
+                  <div>내 주위 이웃들의 게시글</div>{" "}
                 </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
 
-        <div class=" mx-auto w-full max-w-[700px]">
+          <div class=" mx-auto w-full max-w-[700px]">
             <div className="m-3 flex justify-between">
-            <div className="mb-3 block text-base font-medium text-[#6B7280]"></div>
+              <div className="mb-3 block text-base font-medium text-[#6B7280]"></div>
             </div>
             <div className="">
-            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-4">
                 {mainPosts.map((content) => {
-                return (
+                  return (
                     <div class="mb-3">
-                    <PostCardnew
+                      <PostCardnew
                         key={content.id}
                         post={{
-                        content,
+                          content,
                         }}
-                    />
+                      />
                     </div>
-                );
+                  );
                 })}
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div>) :
-        (<div>loading...</div>)}
-        </div>
+      ) : (
+        <div>loading…</div>
+      )}
+    </div>
   );
 };
 export default UserNearPost;
-
